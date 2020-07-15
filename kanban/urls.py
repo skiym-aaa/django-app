7d4680dbd152403814b96925276e1b31c314b0ad
skiym-aaa <y.sakiyama3216@gmail.com>
2020-07-15 18:26:21 +0900

    [Add] リスト機能

--- kanban/urls.py
+++ kanban/urls.py
@@ -7,7 +7,12 @@ app_name = "kanban"
 urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
-    path("signup/", views.signup, name='signup'),
-    path("users/<int:pk>/", views.UserDetailView.as_view(), name='users_detail'),
+    path("signup/", views.signup, name="signup"),
+    path("users/<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
     path("users/<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
+    path("lists/", views.ListListView.as_view(), name="lists_list"),
+    path("lists/create/", views.ListCreateView.as_view(), name="lists_create"),
+    path("lists/<int:pk>/", views.ListDetailView.as_view(), name="lists_detail"),
+    path("lists/<int:pk>/update/", views.ListUpdateView.as_view(), name="lists_update"),
+    path("lists/<int:pk>/delete/", views.ListDeleteView.as_view(), name="lists_delete"),
 ]
