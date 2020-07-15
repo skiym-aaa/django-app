c6c929dfd2466c490eeb921f52891dde9af03cf2
skiym-aaa <y.sakiyama3216@gmail.com>
2020-07-15 15:42:39 +0900

    [Add] ユーザープロフィール機能

--- kanban/views.py
+++ kanban/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, resolve_url
+from django.views.generic import DetailView, UpdateView
+from .mixins import OnlyYouMixin
+from .forms import UserForm
+
 
 def index(request):
     return render(request, "kanban/index.html")
@@ -24,3 +30,15 @@ def signup(request):
         "form": form
     }
     return render(request, 'kanban/signup.html', context)
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = "kanban/users/detail.html"
+
+class UserUpdateView(OnlyYouMixin, UpdateView):
+    model = User
+    template_name = "kanban/users/update.html"
+    form_class = UserForm
+
+    def get_success_url(self):
+        return resolve_url('kanban:users_detail', pk=self.kwargs['pk'])
